3363c2e64186f1166a119f4385f5384a09a392d6
oğuzhan <40310832+oguzhancirak@users.noreply.github.com>
2024-02-05 23:23:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1 = new Person() { FirstName = "oğuzhan",LastName="Çırak",NationalIdentity=123456,DateOfBirthYear=1996 };
+        PttManager pttManager = new PttManager(new PersonManager());
+       
+        pttManager.GiveMask(person1);
+        
+      
+    }
+}
